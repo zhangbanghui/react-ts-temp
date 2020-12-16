c0d3d067daa2833f401a7c9ff9afb5ba7ac9b650
zhangbanghui <banghui.zhang@d3j.io>
2020-12-16 10:42:48 +0800

    feat: init commit

--- src/index.tsx
+++ src/index.tsx
@@ -1,13 +1,12 @@
 import React from 'react';
 import ReactDOM from 'react-dom';
 import './index.css';
-import App from './App';
 import reportWebVitals from './reportWebVitals';
 
+const App = () => (<h2>App</h2>)
+
 ReactDOM.render(
-  <React.StrictMode>
-    <App />
-  </React.StrictMode>,
+  <App />,
   document.getElementById('root')
 );
 
